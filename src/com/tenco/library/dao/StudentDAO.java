84c1b30ca13ebcf80ae91c0c1c924b3353b6c2ec
hewak <dnrdlwlsdl@gmail.com>
2026-04-06 17:03:57 +0900

    feat : StudentDAO 예습

--- src/com/tenco/library/dao/StudentDAO.java
+++ src/com/tenco/library/dao/StudentDAO.java
@@ -1,4 +1,34 @@
 package com.tenco.library.dao;
 
+import com.tenco.library.util.DatabaseUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class StudentDAO {
+    // 학생 등록
+    public int addStudent(){
+        String sql = """
+                INSERT INTO student(name) value (?,?)
+                """;
+
+        try (Connection conn = DatabaseUtil.getConnection()) {
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,"name");
+            pstmt.setString(2,"student_id");
+
+            int rows = pstmt.executeUpdate();
+            return rows;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // 목록 조회
+
+
+
+
 }
