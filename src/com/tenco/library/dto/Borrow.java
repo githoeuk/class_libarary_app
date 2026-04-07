51b45de76d1a192262073489b7a558f63afd8c6b
hewak <dnrdlwlsdl@gmail.com>
2026-04-07 15:06:21 +0900

    feat: borrowDAO,borrow 구현

--- src/com/tenco/library/dto/Borrow.java
+++ src/com/tenco/library/dto/Borrow.java
@@ -16,7 +16,7 @@ public class Borrow {
     private int id;
     private int book_id;
     private int student_id;
-    private LocalDate borrowDate;
+    private LocalDate borrowDate; // sql에서는 date타입이다.
     private LocalDate returnDate;
 
 }
